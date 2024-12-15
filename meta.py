fa1564a9e6f373b03e4f8fe7a05f42d154bd28ca
ohnas <ohnas12@gmail.com>
2024-12-15 15:37:21 +0900

    create_meta_table

--- meta.py
+++ meta.py
@@ -3,24 +3,25 @@ import os
 import json
 import requests
 
+
 load_dotenv()
 
 META_ACT_ID_TTC = os.getenv("META_ACT_ID_TTC")
 META_APP_ACCESSTOKEN = os.getenv("META_APP_ACCESSTOKEN")
 ver = "v21.0"
-insights = "account_currency,account_id,account_name,campaign_id,campaign_name,adset_id,adset_name,ad_id,ad_name,date_start,clicks,conversions,cost_per_inline_link_click,cpm,cpp,ctr,frequency,impressions,purchase_roas,reach,spend,website_ctr,website_purchase_roas,actions,action_values"
+insights = "account_currency,account_id,account_name,campaign_id,campaign_name,adset_id,adset_name,ad_id,ad_name,objective,spend,cost_per_inline_link_click,impressions,reach,frequency,cpm,cpp,created_time,updated_time,actions,action_values"
 url = f"https://graph.facebook.com/{ver}/act_{META_ACT_ID_TTC}/insights"
 params = {
     "fields": insights,
     "access_token": META_APP_ACCESSTOKEN,
     "level": "ad",
-    "breakdowns": "age,gender",
     "time_range": json.dumps(
         {
             "since": "2023-09-11",
             "until": "2023-09-11",
         }
     ),
+    "action_breakdowns": ["action_type"],
     "limit": 100,
 }
 
@@ -36,6 +37,146 @@ while url:
         print(f"Error: {response.status_code}, {response.text}")
         break
 
+
 print(f"Total records fetched: {len(results)}")
-for r in results:
-    print(r)
+
+meta_list = []
+if results:
+    for result in results:
+        dic = {
+            "account_currency": result["account_currency"],
+            "account_id": result["account_id"],
+            "account_name": result["account_name"],
+            "campaign_id": result["campaign_id"],
+            "campaign_name": result["campaign_name"],
+            "adset_id": result["adset_id"],
+            "adset_name": result["adset_name"],
+            "ad_id": result["ad_id"],
+            "ad_name": result["ad_name"],
+            "objective": result["objective"],
+            "spend": result["spend"],
+            "cost_per_inline_link_click": result.get("cost_per_inline_link_click"),
+            "impressions": result["impressions"],
+            "reach": result["reach"],
+            "frequency": result["frequency"],
+            "cpm": result["cpm"],
+            "cpp": result["cpp"],
+            "ad_created_time": result["created_time"],
+            "ad_updated_time": result["updated_time"],
+            "date_start": result["date_start"],
+            "date_stop": result["date_stop"],
+            "like": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "like"
+                ),
+                None,
+            ),
+            "comment": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "comment"
+                ),
+                None,
+            ),
+            "onsite_conversion.post_save": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "onsite_conversion.post_save"
+                ),
+                None,
+            ),
+            "post_reaction": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "post_reaction"
+                ),
+                None,
+            ),
+            "post": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "post"
+                ),
+                None,
+            ),
+            "video_view": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "video_view"
+                ),
+                None,
+            ),
+            "post_engagement": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "post_engagement"
+                ),
+                None,
+            ),
+            "initiate_checkout": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "initiate_checkout"
+                ),
+                None,
+            ),
+            "add_to_cart": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "add_to_cart"
+                ),
+                None,
+            ),
+            "purchase": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "purchase"
+                ),
+                None,
+            ),
+            "add_payment_info": next(
+                (
+                    action["value"]
+                    for action in result.get("actions", [])
+                    if action["action_type"] == "add_payment_info"
+                ),
+                None,
+            ),
+            "initiate_checkout_value": next(
+                (
+                    action["value"]
+                    for action in result.get("action_values", [])
+                    if action["action_type"] == "initiate_checkout"
+                ),
+                None,
+            ),
+            "add_to_cart_value": next(
+                (
+                    action["value"]
+                    for action in result.get("action_values", [])
+                    if action["action_type"] == "add_to_cart"
+                ),
+                None,
+            ),
+            "purchase_value": next(
+                (
+                    action["value"]
+                    for action in result.get("action_values", [])
+                    if action["action_type"] == "purchase"
+                ),
+                None,
+            ),
+        }
+        meta_list.append(dic)
+print(meta_list)
