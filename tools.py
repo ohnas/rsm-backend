345ec6b4d13d6bd2596a24c8bc2611a0cf384921
ohnas <ohnas12@gmail.com>
2025-01-10 16:29:42 +0900

    insert_log

--- tools.py
+++ tools.py
@@ -23,3 +23,26 @@ def log_error(e):
     """현재 함수 이름과 오류 메시지를 동적으로 출력"""
     function_name = inspect.currentframe().f_back.f_code.co_name
     print(f"Error in {function_name}: {e}")
+
+
+def insert_log(conn, status, message=None, platform=None, brand=None):
+
+    function_name = inspect.currentframe().f_back.f_code.co_name
+
+    sql = """
+        INSERT INTO logs (function_name, status, message, platform, brand)
+        VALUES (%(function_name)s, %(status)s, %(message)s, %(platform)s, %(brand)s)
+    """
+    with conn.cursor() as cursor:
+        cursor.execute(
+            sql,
+            {
+                "function_name": function_name,
+                "status": status,
+                "message": message,
+                "platform": platform,
+                "brand": brand,
+            },
+        )
+    conn.commit()
+    print(f"Log recorded: {function_name} - {status}")
