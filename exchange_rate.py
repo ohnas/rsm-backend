363ad6087e9948bd92d260b01cfc63d07f9b9742
ohnas <ohnas12@gmail.com>
2025-01-13 16:37:32 +0900

    exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,8 +1,8 @@
 import requests
-from tools import log_error
+from tools import log_error, insert_log
 
 
-def get_krw_exchange_rate(date):
+def get_krw_exchange_rate(conn, date):
     try:
         URL = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/usd.json"
 
@@ -15,6 +15,15 @@ def get_krw_exchange_rate(date):
             "date": data["date"],
             "krw": round(data["usd"]["krw"], 2),  # 소수점 2자리로 반올림
         }
+        insert_log(
+            conn,
+            date,
+            "SUCCESS",
+            "-",
+            "exchange_rate",
+            "-",
+        )
         return exchange_rate_data
     except Exception as e:
         log_error(e)
+        insert_log(conn, date, "FAIL", str(e), "exchange_rate", "-")
