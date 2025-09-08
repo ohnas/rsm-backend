1576bab6a74d2e95f7312e818fdf5b1b4847af91
ohnas <ohnas12@gmail.com>
2025-09-08 13:46:36 +0900

    ohouse_advertisement.py

--- ohouse_advertisement.py
+++ ohouse_advertisement.py
@@ -19,7 +19,7 @@ conn = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
 )
 
-FILE_PATH = "/Users/ohnaseong/Downloads/2025_07.xlsx"
+FILE_PATH = "/Users/ohnaseong/Downloads/2025_08.xlsx"
 WORK_BOOK = load_workbook(FILE_PATH)
 
 
