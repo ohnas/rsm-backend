2ccd9df8ee23bf0df2d7703f36095370cd01b6d4
ohnas <ohnas12@gmail.com>
2025-10-08 13:08:38 +0900

    rocket_growth.py

--- rocket_growth.py
+++ rocket_growth.py
@@ -21,7 +21,7 @@ conn = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
 )
 
-FILE_PATH = "/Users/ohnaseong/Downloads/2025_08_05.xlsx"
+FILE_PATH = "/Users/ohnaseong/Downloads/2025_09_05.xlsx"
 WORK_BOOK = load_workbook(FILE_PATH)
 
 
